877d97d7b103b286e381cef9a33a2cd094fca905
Khaled Hashem <khaled.hashem1@vodafone.com>
2023-08-28 15:29:59 +0300

    solving null element error

--- src/components/nrealGlasses.jsx
+++ src/components/nrealGlasses.jsx
@@ -1,38 +1,52 @@
 import React, { Component } from 'react';
 class NrealGlasses extends Component {
-   componentDidMount(){
-    
-    let m = document.getElementById("Nreal8");
-    if (m) {
-    
-       m.addEventListener("markerFound", (e) => {
-        debugger;
-        document
-          .getElementById("firstGlass")
-          .setAttribute(
-            "animation__scale",
-            `property: scale; to: 3 3 3; loop: false; dur: 2000; easing: easeInOutSine;`
-          );
+    componentDidMount(){
+        const nreal8 = document.getElementById("Nreal8");
+        const nreal17 = document.getElementById("Nreal17");
+        if (nreal8) {
+            nreal8.addEventListener("markerFound", (e) => {
+                debugger;
+                document.getElementById("NrealModel").setAttribute(
+                    "animation__scale",
+                    `property: scale; to: 4 4 4; loop: false; dur: 1000; easing: easeInOutSine;`
+                );
+            });
+            nreal8.addEventListener("markerLost", (e) => {
+                debugger;
+                document.getElementById("NrealModel").setAttribute(
+                    "animation__scale",
+                    `property: scale; to: 0 0 0; loop: false; dur: 1000; easing: easeInOutSine;`
+                );
+            });
+        }
+        if (nreal17) {
+            nreal17.addEventListener("markerFound", (e) => {
+                debugger;
+                document.getElementById("NrealModel").setAttribute(
+                    "animation__scale",
+                    `property: scale; to: 4 4 4; loop: false; dur: 1000; easing: easeInOutSine;`
+                );
+            });
+            nreal17.addEventListener("markerLost", (e) => {
+                debugger;
+                document.getElementById("NrealModel").setAttribute(
+                    "animation__scale",
+                    `property: scale; to: 0 0 0; loop: false; dur: 1000; easing: easeInOutSine;`
+                );
+            });
+        }
+        document.getElementById("NrealModel").on("animationcomplete", function (event) {
+            if (document.getElementById("NrealModel").attr("animation__scale")) {
+                document.getElementById("NrealModel").removeAttr("animation__scale");
+            }
       });
-      m.addEventListener("markerLost", (e) => {
-        debugger;
-        document
-          .getElementById("firstGlass")
-          .setAttribute(
-            "animation__scale",
-            `property: scale; to: 0 0 0; loop: false; dur: 2000; easing: easeInOutSine;`
-          );
-      });
-     
     }
-}
     render() { 
         return (
             <>
                 <a-scene
                 vr-mode-ui="enabled: false;"
                 renderer="logarithmicDepthBuffer: true;"
-                
                 arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
                 >
                     <a-nft
@@ -43,35 +57,26 @@ class NrealGlasses extends Component {
                         smoothCount="10"
                         smoothTolerance=".01"
                         smoothThreshold="5"
-                        
                     >
-                      
                     </a-nft>
-                      <a-entity
-                      id="firstGlass"
-                            gltf-model="url(/Nreal light glasses/Nreal Light.glb)"
-                            scale="0 0 0"
-                          position='-5 -10 -30'
-                            rotation="0 0 0"
-                        >
-                        </a-entity>
                     <a-nft
-                    id="Nreal17"
-                    type="nft"
-                    url="./Nreal glasses 17 - 300 DPI/Nreal glasses 17"
-                    smooth="true"
-                    smoothCount="10"
-                    smoothTolerance=".01"
-                    smoothThreshold="5"
-                >
+                        id="Nreal17"
+                        type="nft"
+                        url="./Nreal glasses 17 - 300 DPI/Nreal glasses 17"
+                        smooth="true"
+                        smoothCount="10"
+                        smoothTolerance=".01"
+                        smoothThreshold="5"
+                    >
+                    </a-nft>
                     <a-entity
-                    gltf-model="./Nreal light glasses/Nreal Light.glb"
-                    scale="40 40 40"
-                    position="0 -20 0"
-                    rotation="270 0 0"
+                        id="NrealModel"
+                        gltf-model="./Nreal light glasses/Nreal Light.glb"
+                        scale="0 0 0"
+                        position='-5 -10 -30'
+                        rotation="0 0 0"
                     >
                     </a-entity>
-                </a-nft>
                     <a-entity camera></a-entity>
                 </a-scene>
             </>
