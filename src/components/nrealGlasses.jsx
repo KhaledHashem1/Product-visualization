de982b0e0db455700bd20adc61b88824919d17e8
KhaledHashem1 <khaled.hashem1@vodafone.com>
2023-08-28 03:02:12 +0300

    adjusting model position

--- src/components/nrealGlasses.jsx
+++ src/components/nrealGlasses.jsx
@@ -21,7 +21,7 @@ class NrealGlasses extends Component {
                         <a-entity
                             gltf-model="./Nreal light glasses/Nreal Light.glb"
                             scale="40 40 40"
-                            position="145 0 -45"
+                            position="145 0 -50"
                             rotation="270 0 0"
                         >
                         </a-entity>
@@ -38,7 +38,7 @@ class NrealGlasses extends Component {
                     <a-entity
                     gltf-model="./Nreal light glasses/Nreal Light.glb"
                     scale="40 40 40"
-                    position="125 0 -15"
+                    position="110 0 -30"
                     rotation="270 0 0"
                     >
                     </a-entity>
