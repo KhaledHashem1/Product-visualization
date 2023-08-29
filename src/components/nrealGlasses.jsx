ecaa808d185a611b9a94a842ef3b0c3e713da2f3
Khaled Hashem <khaled.hashem1@vodafone.com>
2023-08-29 10:27:16 +0300

    changing positions

--- src/components/nrealGlasses.jsx
+++ src/components/nrealGlasses.jsx
@@ -73,7 +73,7 @@ class NrealGlasses extends Component {
                         id="NrealModel"
                         gltf-model="./Nreal light glasses/Nreal Light.glb"
                         scale="0 0 0"
-                        position='0 0 -30'
+                        position='0 -5 -30'
                         rotation="0 0 0"
                     >
                     </a-entity>
